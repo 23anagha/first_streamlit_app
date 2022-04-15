61a86bdeaddb9eee90b2d9dd5a586a68a0e7d20f
Anagha Dhoble <103777653+23anagha@users.noreply.github.com>
2022-04-16 01:15:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
 streamlit.title("My Mom's New Healthy Diner")
-streamlit.header("Breakfast's favourite")
-
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range egg')
-streamlit.text('Avocado Toast')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
